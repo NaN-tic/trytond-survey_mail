9435542151ae52c9432af7fde9441b62a2608c4b
resteve <resteve@zikzakmedia.com>
2015-12-01 01:09:22 +0100

    Tests

--- tests/test_survey_mail.py
+++ tests/test_survey_mail.py
@@ -1,27 +1,14 @@
-#!/usr/bin/env python
-# This file is part of the survey module for Tryton.
+# This file is part of the survey_mail module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains the full
 # copyright notices and license terms.
 import unittest
-import doctest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_view, test_depends
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class SurveyMailCase(unittest.TestCase):
-    '''Test Survey Mail module'''
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module('survey_mail')
-
-    def test0005views(self):
-        '''Test views'''
-        test_view('survey_mail')
-
-    def test0006depends(self):
-        '''Test depends'''
-        test_depends()
+class SurveyMailCase(ModuleTestCase):
+    'Test Survey Mail module'
+    module = 'survey_mail'
 
 
 def suite():
